3cb72465fa46d58a8843015735b04f3f3836582f
uday246r <uday1549.be23@chitkarauniversity.edu.in>
2025-01-25 07:33:21 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id : "child" }, [
-        React.createElement("h1", {}, "This is Namaste React"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id : "child2" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-]);
+//React.createElement => object => HTML Element(when render)
 
-console.log(parent);
+// const parent = React.createElement("div", { id: "parent" }, [
+//     React.createElement("div", { id : "child" }, [
+//         React.createElement("h1", {}, "This is Namaste React"),
+//         React.createElement("h2", {}, "I'm an h2 tag"),
+//     ]),
+//     React.createElement("div", { id : "child2" }, [
+//         React.createElement("h1", {}, "I'm an h1 tag"),
+//         React.createElement("h2", {}, "I'm an h2 tag"),
+//     ]),
+// ]);
+
+// console.log(parent);
+
+
+// JSX
+const jsxHeading =( <h1 id ="heading">
+    Namaste React Using JSX
+    </h1>);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
+
