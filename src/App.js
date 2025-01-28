8ab1d14a033e37cc4abb4265fd277475fdc5265f
uday246r <uday1549.be23@chitkarauniversity.edu.in>
2025-01-28 13:45:37 +0530

    episod-07(part1)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //React.createElement => object => HTML Element(when render)
 
@@ -55,9 +58,26 @@ const AppLayout = () => {
         </div>
     );
 };
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <Error />
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+    {
+        path: "/contact",
+        element: <Contact />,
+    }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(jsxHeading);
-root.render(<AppLayout/>)
+root.render(<RouterProvider router={appRouter}/>)
 
 
