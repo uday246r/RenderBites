d86481151f76f6c2891b85626457e753a1e2353c
uday246r <uday1549.be23@chitkarauniversity.edu.in>
2025-01-28 23:09:26 +0530

    session-07(Image_In_Menu)

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,7 @@
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import {MENU_API} from "../utils/constants"
+import {MENU_API, CDN_URL} from "../utils/constants"
 const RestaurantMenu = () =>{
 
     const [resInfo, setResInfo] = useState(null);
@@ -15,18 +15,23 @@ const RestaurantMenu = () =>{
     const fetchMenu = async()=>{
         const data = await fetch( MENU_API+ resId );
         const json=await data.json();
-        console.log(json.data);
         setResInfo(json?.data);
     };
 
     if(resInfo===null) return <Shimmer/>;
     
-   const {name, cuisines, costForTwo} = resInfo?.cards[2]?.card?.card?.info ;
+   const {name,cloudinaryImageId, cuisines, costForTwo} = resInfo?.cards[2]?.card?.card?.info ;
    const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card ;
 
     return (
         <div className="Menu">
             <h1>{name}</h1>
+            <div className="res-card" style={{ backgroundColor: "#f0f0f0"}}>
+            <img className="res-logo" 
+             alt="res-logo"
+             src={CDN_URL + cloudinaryImageId}
+             />
+                </div>
             <p>{cuisines.join(", ")} - {costForTwo}</p>
             <h2>Menu</h2>
             <ul>
