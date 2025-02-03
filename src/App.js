83a167c906f7db10473bc37b1102979a174641f8
uday246r <uday1549.be23@chitkarauniversity.edu.in>
2025-02-03 13:29:56 +0530

    session-09(complete)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Shimmer from "./components/Shimmer";
 import RestaurantMenu from "./components/RestaurantCard";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/RestaurantMenu";
+// import Grocery from "./components/Grocery";
 
 //React.createElement => object => HTML Element(when render)
 
@@ -51,6 +53,7 @@ import RestaurantMenu from "./components/RestaurantMenu";
 // );
 
 
+const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
     return(
@@ -77,6 +80,10 @@ const appRouter = createBrowserRouter([
             {
                 path: "/contact",
                 element: <Contact />,
+            },
+            {
+                path: "/grocery",
+                element: <Suspense fallback={<Shimmer/>}><Grocery /></Suspense>,
             },
             {
                 path: "/restaurant/:resId",
---
+++ src/components/Grocery.js
@@ -0,0 +1,10 @@
+const Grocery = () =>{
+return ( 
+        <h1>
+        {" "}
+        Our grocery onlinestore, and we have a lot of child components inside this web page!!!
+    </h1>
+    );
+};
+
+export default Grocery
